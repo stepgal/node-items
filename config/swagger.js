a93e29d0d181d505b9a392f89d94ec328676f789
Stepan Galoyan <stepan@gmail.com>
2020-06-18 23:40:29 +0400

    updated swagger

--- config/swagger.js
+++ config/swagger.js
@@ -1,6 +1,4 @@
-const mongoose = require('mongoose');
 const swaggerJSDoc = require("swagger-jsdoc");
-const m2s = require('mongoose-to-swagger');
 const definitions = require("../schemas/definitions");
 const SWAGGER = require("./");
 
